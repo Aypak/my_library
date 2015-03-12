8e538a0efe3593cdd33520b3ad9a01bbaf1f54f4
Kapya <kapsakala@gmail.com>
2015-03-12 09:51:00 +0200

    index, new, and create actions

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,24 @@
+class BooksController < ApplicationController
+	def index
+		@books = Book.all
+	end
+
+	def new
+		@book = Book.new
+	end
+
+	def create
+		@book = Book.new(book_params)
+		if @book.save
+			redirect_to root_path
+		else
+			flash[:notice]='Could not create new book'
+		end
+
+	end
+	
+	private 
+	def book_params
+		params.require(:book).permit(:title, :author, :summary)
+	end
+end
