24c1ff5e3d0c84d496bafa11463192b8e2f7eeeb
Kapya <kapsakala@gmail.com>
2015-03-13 09:35:38 +0200

    removed default values

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,8 +1,6 @@
 class Book < ActiveRecord::Base
 	belongs_to :person
-	def initialize(attributes=nil)
-	    attr_with_defaults = {title: "unknown title", author: "unknown author"}.merge(attributes)
-	    super(attr_with_defaults)
-	end
+	validates :title, :author, :price, presence: true
+	
 	
 end
--- app/models/person.rb
+++ app/models/person.rb
@@ -1,7 +1,4 @@
 class Person < ActiveRecord::Base
 	has_many :books
-  def initialize(attributes=nil)
-    attr_with_defaults = {name: "unknown name", maximum_books: 3}.merge(attributes)
-    super(attr_with_defaults)
-  end
+  	validates :name, presence: true
 end
